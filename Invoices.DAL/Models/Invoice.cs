17ad0783bf8dbdb8411057e9f5edd36a0d2048ac
Alx.AhS <alex.ahusoto@gmail.com>
2022-11-26 14:53:37 -0500

    Controller check

--- Invoices.DAL/Models/Invoice.cs
+++ Invoices.DAL/Models/Invoice.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,9 @@ namespace DAL.Models
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int InvoiceID { get; set; }
-        [Column(TypeName = "nvarchar(16)")]
+        [Column(TypeName = "nvarchar(30)")]
         public DateTime InvoiceDate { get; set; }
-        
+
         [Column(TypeName = "nvarchar(10)")]
         public string? InvoiceNumber { get; set; }
     }
