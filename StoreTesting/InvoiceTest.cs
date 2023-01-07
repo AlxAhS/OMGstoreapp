12071356969b7f930263d25a74f67ef84aa20b11
Alx.AhS <alex.ahusoto@gmail.com>
2023-01-07 16:18:17 -0500

    Unit test for controllers

---
+++ StoreTesting/InvoiceTest.cs
@@ -0,0 +1,15 @@
+using Invoices.API.Controllers;
+
+namespace StoreTesting
+{
+    public class InvoiceTest
+    {
+        private readonly InvoiceController _invoiceController;  
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ StoreTesting/ProductTest.cs
@@ -0,0 +1,15 @@
+using Invoices.API.Controllers;
+
+namespace StoreTesting
+{
+    public class ProductTest
+    {
+        private readonly ProductController _productcontroller;
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
