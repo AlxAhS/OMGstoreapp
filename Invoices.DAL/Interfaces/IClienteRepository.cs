fd2875c7583560d1301f56c48079d7a4a0c28a77
Alx.AhS <alex.ahusoto@gmail.com>
2023-01-19 16:29:32 -0500

    Client interfaces was created

--- Invoices.DAL/Interfaces/IClienteRepository.cs
+++ Invoices.DAL/Interfaces/IClienteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Invoices.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,17 @@ using System.Threading.Tasks;
 
 namespace Invoices.DAL.Interfaces
 {
-    internal interface IClienteRepository
+    public interface IClienteRepository
     {
+        IEnumerable<Client> GetAll();
+
+        Client GetbyId(int id);
+
+        Client Add(Client entity);
+
+        Client Update(int id);
+
+        void Delete(int id);
+
     }
 }
